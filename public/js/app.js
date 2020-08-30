ad745a35b74bdcfd599ec6ceff9e3a02048caa10
Arfiandimas <arfiandimas1929@gmail.com>
2020-08-30 23:13:17 +0700

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,7 @@ const pencarian = (location) => {
     messageOne.textContent = 'Loading ....'
     messageTwo.textContent = ''
     
-    fetch(`http://localhost:3000/weather?location=${location}`).then((response) => {
+    fetch(`/weather?location=${location}`).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = data.error
